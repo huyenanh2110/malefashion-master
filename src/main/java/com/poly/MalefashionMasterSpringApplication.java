2aab3387f9ae622e96ba82df48b5e0aa8278d9bb
huyenanh2110 <huyenanhbui021021@gmail.com>
2024-07-20 18:27:14 +0700

    connect db with posgress

--- src/main/java/com/poly/MalefashionMasterSpringApplication.java
+++ src/main/java/com/poly/MalefashionMasterSpringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MalefashionMasterSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MalefashionMasterSpringApplication.class, args);
     }
